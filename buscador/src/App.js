fc6b87afa183021f7a607c9cf5a77baf9565d838
Edson-Padilha <edsonp.bsi@gmail.com>
2024-02-16 22:36:57 -0300

    Finalizando estilização

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,8 +1,26 @@
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Teste projeto busca dep</h1>
+    <div className="container">
+      <h1 className="title">Busca Cep</h1>
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep..."
+        />
+        <button className="buttonSearch">
+          <FiSearch size={25} color='#FFF'/>
+        </button>
+      </div>
+        <main className='main'>
+          <h2>CEP: 89254-230</h2>
+          <span>Rua: Emilio Klitzke</span>
+          <span>Complemento: Próximo a sorveteria</span>
+          <span>Bairro: Três Rios do Sul</span>
+          <span>Cidade: Jaraguá do Sul - SC</span>
+        </main>
     </div>
   );
 }
